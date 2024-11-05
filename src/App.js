873ca9b1c501d298b264be0eed521d5e2dd2c830
Paul2000-2000 <alexapaul2000@yahoo.com>
2024-11-05 14:14:22 +0200

    hero done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./views/Hero/Hero";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
