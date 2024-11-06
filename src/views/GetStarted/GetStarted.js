07be1dde2b0a38fcecfd33f5c4aae3f8f82e79d9
Paul2000-2000 <alexapaul2000@yahoo.com>
2024-11-06 13:44:29 +0200

    finish getstarted

---
+++ src/views/GetStarted/GetStarted.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <div className="getstarted">
+      <div className="getstarted-content">
+        <h1 className="getstarted-content-title">Get started with Paul</h1>
+        <div className="getstarted-content-desc">
+          <p>Subscribe and find super attractive price quotes from us.</p>
+          <p> Find your residence soon</p>
+        </div>
+        <button className="getstarted-content-but">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
